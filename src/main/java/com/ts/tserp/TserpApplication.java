337ba757f011196b966943b01e43b386d45f3e7c
vinod <borse002@gmail.com>
2022-11-23 12:21:24 +0530

    Initial project

---
+++ src/main/java/com/ts/tserp/TserpApplication.java
@@ -0,0 +1,13 @@
+package com.ts.tserp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TserpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TserpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ts/tserp/TserpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.tserp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TserpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
