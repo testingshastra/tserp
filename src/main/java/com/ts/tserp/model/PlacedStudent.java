4b939e32736586a111d601b8ca9f881cc301fb8c
Vaishnavi Ahirrao <vaishnaviahirrao10497@gmail.com>
2022-11-30 15:31:50 +0530

    modified the file

--- src/main/java/com/ts/tserp/model/PlacedStudent.java
+++ src/main/java/com/ts/tserp/model/PlacedStudent.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.sun.istack.NotNull;
+
 @Entity(name="Recent placement")
 public class PlacedStudent {
 	
@@ -13,69 +15,72 @@ public class PlacedStudent {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 		private int id;
 	
-	@Column(name = "Student name")
+	
+	@NotNull
+	@Column(name = "Student_name")
+	
 		private String student_name;
 	
-	@Column(name ="Company name")
+	@Column(name ="Company_name")
 		private String company_name;
 	
-		private String emailId;
-		
-		private int phone_no;
-		
+	    private int salary;
+     
+     @NotNull  
+	    private  int phone_number;
+	
 		private String position;
-		
-		private int salary;
-		
-		public int getSalary() {
-			return salary;
-		}
-		public void setSalary(int salary) {
-			this.salary = salary;
-		}
+
 		public int getId() {
 			return id;
 		}
+
 		public void setId(int id) {
 			this.id = id;
 		}
-		
+
 		public String getStudent_name() {
 			return student_name;
 		}
+
 		public void setStudent_name(String student_name) {
 			this.student_name = student_name;
 		}
+
 		public String getCompany_name() {
 			return company_name;
 		}
-				public void setCompany_name(String company_name) {
+
+		public void setCompany_name(String company_name) {
 			this.company_name = company_name;
 		}
-		public String getEmailId() {
-			return emailId;
+
+		public int getSalary() {
+			return salary;
 		}
-		public void setEmailId(String emailId) {
-			this.emailId = emailId;
+
+		public void setSalary(int salary) {
+			this.salary = salary;
 		}
-		public int getPhone_no() {
-			return phone_no;
+
+		public int getPhone_number() {
+			return phone_number;
 		}
-		public void setPhone_no(int phone_no) {
-			this.phone_no = phone_no;
+
+		public void setPhone_number(int phone_number) {
+			this.phone_number = phone_number;
 		}
+
 		public String getPosition() {
 			return position;
 		}
+
 		public void setPosition(String position) {
 			this.position = position;
 		}
-		@Override
-		public String toString() {
-			return "PlacedStudent [id=" + id + ", student_name=" + student_name + ", company_name=" + company_name
-					+ ", emailId=" + emailId + ", phone_no=" + phone_no + ", position=" + position + ", salary="
-					+ salary + "]";
-		}
+		
+	
+	
 		
 		
 		
