2e076fa1191ece2f84d908c3505d4f921463ce53
sagargaike1 <sagargaike123@gmail.com>
2022-11-30 11:32:59 +0530

    Added the Batch_Announcement Java file for creation of batch announcement tabel

---
+++ src/main/java/com/ts/tserp/model/Batch_Announcement.java
@@ -0,0 +1,73 @@
+package com.ts.tserp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "batch_announcement")
+public class Batch_Announcement {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	private String course_title;
+
+	private String start_date;
+
+	private double start_time;
+
+	private String mode;
+
+	private String trainer_name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCourse_title() {
+		return course_title;
+	}
+
+	public void setCourse_title(String course_title) {
+		this.course_title = course_title;
+	}
+
+	public String getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(String start_date) {
+		this.start_date = start_date;
+	}
+
+	public double getStart_time() {
+		return start_time;
+	}
+
+	public void setStart_time(double start_time) {
+		this.start_time = start_time;
+	}
+
+	public String getMode() {
+		return mode;
+	}
+
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+
+	public String getTrainer_name() {
+		return trainer_name;
+	}
+
+	public void setTrainer_name(String trainer_name) {
+		this.trainer_name = trainer_name;
+	}
+
+}
